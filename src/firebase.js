1d8ac259f8a2dd000232ef64c618dd5235b0aeff
mmc100 <mmc100@mmc100s-Mac-Studio.local>
2026-05-19 23:01:32 +0900

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,39 +1,16 @@
-import { initializeApp }
-from "firebase/app";
-
-import {
-getFirestore
-}
-from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-apiKey:
-"너 apiKey",
-
-authDomain:
-"idol-mv-archive.firebaseapp.com",
-
-projectId:
-"idol-mv-archive",
-
-storageBucket:
-"idol-mv-archive.appspot.com",
-
-messagingSenderId:
-"너 sender",
-
-appId:
-"너 appId",
-
+  apiKey: "AIzaSyA9_QochfHnsxSO0Kjvt_2qTnCV5QUmYVg",
+  authDomain: "idol-mv-archive.firebaseapp.com",
+  projectId: "idol-mv-archive",
+  storageBucket: "idol-mv-archive.firebasestorage.app",
+  messagingSenderId: "221694301572",
+  appId: "1:221694301572:web:cccb1b12b3a49a16e239d1",
+  measurementId: "G-N4QGPPN57J",
 };
 
-const app =
-initializeApp(
-firebaseConfig
-);
+const app = initializeApp(firebaseConfig);
 
-export const db =
-getFirestore(
-app
-);
+export const db = getFirestore(app);
